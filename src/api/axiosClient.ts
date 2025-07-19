d30d74507eaca26ee614c800a48602877ef48f1f
Dulanka Katugampala <dulanka.katugampala@gmail.com>
2025-07-20 05:10:35 +0530

    Add Content-Type header to axiosClient configuration for JSON requests

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -4,6 +4,9 @@ import { environmentVariables } from '../environmentVariables';
 const options = {
   baseURL: environmentVariables.ApiUrl,
   timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 };
 
 export const axiosClient = axios.create(options);
